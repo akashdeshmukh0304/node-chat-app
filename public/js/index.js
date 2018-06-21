1a47f470aaf356c1bcb7e123bf674ed37f39daff
akash <akash@localhost.localdomain>
2018-06-21 08:40:49 +0530

    Add new message and create message events

---
+++ public/js/index.js
@@ -0,0 +1,18 @@
+var socket = io();
+
+socket.on('connect', function () {
+	console.log('Connected to server');
+
+	socket.emit('createMessage', {
+		from: "Jay",
+		text: "Hey hiee bro"
+	});
+});
+
+socket.on('disconnect', function () {
+	console.log('Disconnected from server');
+});
+
+socket.on('newMessage', function (message) {
+	console.log('New message received', message);
+})
--- server/server.js
+++ server/server.js
@@ -14,6 +14,16 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
 	console.log('New user connected.')
 
+	socket.emit('newMessage', {
+		from: "Akash",
+		text: "Hey hiee",
+		createdAt: "123"
+	});
+
+	socket.on('createMessage', function (messageBody) {
+		console.log(messageBody);
+	});
+
 	socket.on('disconnect', () => {
 		console.log('User was diconnected.')
 	});
