4a90837a1f79610505823c248624732dac6e483b
akash <akash@localhost.localdomain>
2018-07-12 08:43:00 +0530

    Scroll to bottom is user is close to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,22 @@
 var socket = io();
 
+function scrollToBottom() {
+	// Selectors
+	var messages = $('#messages');
+	var newMessage = messages.children('li:last-child');
+	// Heights
+	var clientHeight = messages.prop('clientHeight');
+	var scrollTop = messages.prop('scrollTop');
+	var scrollHeight = messages.prop('scrollHeight'); 
+	var newMessageHeight = newMessage.innerHeight();
+	var lastMessageHeight = newMessage.prev().innerHeight();
+
+	if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+		messages.scrollTop(scrollHeight);
+	}
+
+}
+
 socket.on('connect', function () {
 	console.log('Connected to server');
 });
@@ -17,6 +34,7 @@ socket.on('newMessage', function (message) {
 		createdAt: formattedTime
 	});
 	jQuery('#messages').append(html);
+	scrollToBottom();
 });
 
 jQuery('#message-form').on('submit', function (e) {
@@ -42,6 +60,7 @@ socket.on('newLocationMessage', function (message) {
 	});
 
 	jQuery('#messages').append(html);
+	scrollToBottom();
 });
 
 var locationButton = $('#send-location');
